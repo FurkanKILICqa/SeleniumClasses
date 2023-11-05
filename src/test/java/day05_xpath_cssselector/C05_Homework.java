cbd0586849e6e01f8cb3b1d4a2214dbb128443fa
Furkan Kılıc <fkrx928@gmail.com>
2023-11-05 23:37:55 +0300

    my commit

---
+++ src/test/java/day05_xpath_cssselector/C05_Homework.java
@@ -0,0 +1,4 @@
+package day05_xpath_cssselector;
+
+public class C05_Homework {
+}
---
+++ src/test/java/day05_xpath_cssselector/hackerrankTest.java
@@ -0,0 +1,47 @@
+package day05_xpath_cssselector;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class hackerrankTest {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.hackerrank.com/");
+
+        driver.findElement(By.partialLinkText("Sign up")).click();
+
+        driver.findElement(By.className("//div[@class='active'])[1]")).click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
